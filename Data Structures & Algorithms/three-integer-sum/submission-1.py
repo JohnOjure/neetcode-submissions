6afc41c68453909b347ba922f0d6e0037be17924
Oluwasetemi Kunle-Ajayi <johnajayi008@gmail.com>
2026-07-17 19:10:40 +0100

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        output = set()
+
+        for i in range(len(nums)):
+            hm = {}
+            target = -1 * nums[i]
+
+            for j in range(i + 1, len(nums)):
+                if target - nums[j] in hm:
+                    output.add(tuple(sorted([nums[i], target - nums[j], nums[j]])))
+                else:
+                    hm[nums[j]] = j
+        
+        return list(map(list, output))
