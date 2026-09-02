95eb7f532ed544cdfc64981a9946984d2c98a310
Oluwasetemi Kunle-Ajayi <johnajayi008@gmail.com>
2026-09-02 17:54:02 +0100

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,38 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+
+        output = []
+
+        hm = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
+        }
+
+        def backtrack(start, current_path):
+            if len(current_path) == len(digits):
+                output.append(current_path[:])
+                return 
+
+            #choose
+            for char in hm[digits[start]]: 
+                #for character at every level of the 
+                #particular digit we're considering, choose
+                #one of the possible characters that it could reporesent 
+                current_path += char
+
+                backtrack(start + 1, current_path)
+
+                current_path = current_path[:-1]
+
+        backtrack(0, "")
+
+        return output
+
+
