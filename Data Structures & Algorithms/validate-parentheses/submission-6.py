0724c6288a0412e854775658a04d11f247a4b29f
Oluwasetemi Kunle-Ajayi <johnajayi008@gmail.com>
2026-09-18 18:54:45 +0100

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        stack = []
+        complement = {
+            ")": "(",
+            "]": "[",
+            "}": "{"
+        }
+
+        for elem in s:
+
+            if elem not in complement:
+                stack.append(elem)
+
+            else: #if elem is a closing parenthesis
+                if not stack or stack[-1] != complement[elem]:
+                    return False
+                else:
+                    stack.pop()
+
+        return not stack
+        
