57e0425fb5d8f96e796cb0d77b9956739ca9f8ab
Oluwasetemi Kunle-Ajayi <johnajayi008@gmail.com>
2026-09-18 18:55:23 +0100

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        complement = {
+            ")": "(",
+            "]": "[",
+            "}": "{"
+        }
+
+        for elem in s:
+            if elem not in complement:
+                stack.append(elem)
+
+            else: #if elem is a closing parenthesis
+                if not stack or stack[-1] != complement[elem]:
+                    return False
+                else:
+                    stack.pop()
+
+        return not stack
+        
